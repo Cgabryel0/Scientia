89f2d69c473fbe51a151505c1e29c3da06e58df4
Cgabryel0 <cgabryel0@gmail.com>
2026-08-25 22:09:10 +0000

    correções!

--- frontend/src/servicos/pesquisadorService.js
+++ frontend/src/servicos/pesquisadorService.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { montarConsulta, requisitar } from './api.js';
-
-export function listar(filtros) {
-  return requisitar(`/pesquisadores${montarConsulta(filtros)}`);
-}
-=======
 import { montarConsulta, requisitar } from './api.js';
 
 export function listar(filtros) {
@@ -14,4 +7,3 @@ export function listar(filtros) {
 export function obterPorId(id) {
   return requisitar(`/pesquisadores/${id}`);
 }
->>>>>>> a207049 (feat(frontend): adiciona páginas de publicações, pesquisadores e rotas principais)
